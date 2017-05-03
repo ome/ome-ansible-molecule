350a9f5131cc0018e1bcd1e0ef79e7292f0ea2c7
jmoore <josh@glencoesoftware.com>
2017-05-03 17:57:01 +0200

    Simplify update.py

--- update.py
+++ update.py
@@ -2,27 +2,22 @@
 #
 # This script should be called with a list, one per line
 # of role names that should be registered in .travis.yml
+# (e.g. from ./repos.sh)
 #
 
-from fileinput import input
-from sys import stdin
+import re
+import sys
 
-BEGIN = 1
-UNPRINTED = 2
-PRINTED = 3
-END = 4
-state = 1
-for line in input([".travis.yml"]):
-    if line.startswith("### GENERATED ###"):
-        state += 1
-        print line,
-    elif state in (BEGIN, END):
-        print line,
-    elif state == UNPRINTED:
-        state += 1
-        roles = stdin.read().strip().split("\n")
-        print "env:"
-        for role in roles:
-            if role.startswith("ansible-role"):
-                role = role[13:]
-            print "- ROLE=%s" % role
+TAG = "###GENERATED###"
+
+with open(".travis.yml", "r") as fin:
+    txt = fin.read()
+
+roles = sys.stdin.read().strip().split("\n")
+replacement = "%s\nenv:\n" % TAG
+for role in roles:
+    if role.startswith("ansible-role"):
+        role = role[13:]
+    replacement += "- ROLE=%s\n" % role
+replacement += "%s" % TAG
+print re.sub("%s.*?%s" % (TAG, TAG), txt, replacement, flags=re.DOTALL),
